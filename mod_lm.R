51ae477039f5ae10a71b13e1a33494645fa40461
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-11-18 15:24:45 -0500

    meeting with Steve

--- mod_lm.R
+++ mod_lm.R
@@ -3,11 +3,11 @@ commandEnvironments()
 sourceFiles()
 
 ## Fit lm model
-mod_unscaled <- lm(pred1 ~ x1 + x2, data = sim_df)
+mod_unscaled <- lm(y1 ~ x1 + x2, data = sim_df)
 summary(mod_unscaled)
 
 ## Fit unscaled lm model
-mod_scaled <- lm(pred1 ~ x1s + x2s, data = sim_df)
+mod_scaled <- lm(y1 ~ x1s + x2s, data = sim_df)
 summary(mod_scaled)
 
 saveVars(mod_unscaled
--- sims_data.R
+++ sims_data.R
@@ -8,7 +8,7 @@ commandEnvironments()
 
 set.seed(7902)
 
-people <- 500 # Number of simulations to run
+people <- 500 # sample size
 
 # Beta values
 y1_beta0 <- 0.3
@@ -20,26 +20,23 @@ e1 <- 1
 # Predictors
 x1 <- seq(1, 9, length.out = people)
 summary(x1)
-x2 <- sample(runif(people*people, 10, 100), people)
+x2 <- runif(people, 10, 100)
 summary(x2)
 
 ## Error terms
 y1_eps <- e1*rnorm(people)
 
-## Linear predictor
-XB <- data.frame(x1 = x1
+## Make data frame
+sim_df <- data.frame(x1 = x1
 	, x2 = x2
 	, x1s = drop(scale(x1)) # Scaled
 	, x2s = drop(scale(x2)) # Scaled
-	, pred1 = y1_beta0 + y1_beta1 * x1 + y1_beta2 * x2 + y1_eps
+	, y1 = y1_beta0 + y1_beta1 * x1 + y1_beta2 * x2 + y1_eps
 )
 
-## Simulated data
-sim_df <- (XB
-	%>% mutate(
-		y1 = rbinom(people, 1, plogis(pred1))
-	)
-)
+sim_df <- (sim_df %>% mutate(
+	y1bin = rbinom(people, 1, plogis(y1))
+))
 
 parm_list <- list(y1_beta0 = y1_beta0
 	, y1_beta1 = y1_beta1
