8ee71f34327716b35ad5a08254b19fbe14d882b8
cygubicko <cygu@aims.ac.za>
2020-12-18 18:20:23 -0500

    Coefficient scaling horrible

--- effectsfuns.R
+++ effectsfuns.R
@@ -148,7 +148,7 @@ varfun <- function(vcol, steps){
 #'
 #' @export 
 #'
-## Extract assign for model object esp. for glmmTMB
+
 extract_assign <- function(mod){
 	mat <- model.matrix(mod)
 	modframe <- model.frame(mod, drop.unused.levels = TRUE)
@@ -164,7 +164,12 @@ extract_assign <- function(mod){
 	return(list(assign = assign, modmat = mat, modframe = modframe))
 }
 
-## zero out non-focal covariances from matrix
+#' Zero out non-focal covariances from matrix
+#'
+#' @param m model object
+#' @param focal_vars variables appearing on the right side of the formula in the fitted model
+#'
+#' @export
 zero_vcov <- function(m, focal_vars) {
 	assign <- extract_assign(m)$assign
 	check_vars <- assign %in% focal_vars
@@ -180,4 +185,54 @@ zero_vcov <- function(m, focal_vars) {
 	return(v)
 }
 
+#' Scale model coefficients from unscaled coefficients
+#'
+#' @param mod fitted model object
+#' @param scale_vars variables to scale. This can include response variable
+#' @param tfun transformation function. Default is scale
+#'
+#' @export
 
+scalecoef <- function(mod, scale_vars, tfun = function(x){scale(x, center = TRUE, scale = TRUE)}){
+	betahat <- extractcoef(mod)
+	btemp <- betahat
+	mod_objs <- extract_assign(mod)
+	vnames <- mod_objs$assign
+	check_vars <- vnames %in% scale_vars
+	if (any(!check_vars)) {
+		if (all(!check_vars)) stop("All specified variables not in the model.")
+		out_vars <- scale_vars[!scale_vars %in% vnames]
+		if (length(out_vars)){
+			warning(c(paste(out_vars, collapse = ", "), " not in the fitted model. It (they) will be ignored."))
+		}
+	}
+#	scale_vars <- scale_vars[scale_vars %in% vnames]
+#	scale_coefs <- grep(paste0(scale_vars, collapse="|"), names(vnames), value = TRUE)
+#	noscale_coefs <- setdiff(names(vnames), scale_coefs)
+	scale_coefs <- names(vnames)[check_vars]
+	noscale_coefs <- names(vnames)[!check_vars]
+	modMat <- tfun(mod_objs$modmat[, scale_coefs, drop = FALSE])
+	scale_sd <- c(attr(modMat, "scaled:scale"))
+	scale_mu <- c(attr(modMat, "scaled:center"))
+	if (length(noscale_coefs)){
+		noscale_sd <- rep(1, length(noscale_coefs))
+		noscale_mu <- rep(0, length(noscale_coefs))
+		sds <- c(noscale_sd, scale_sd)
+		names(sds) <- c(noscale_coefs, names(scale_mu))
+		mus <- c(noscale_mu, scale_mu)
+		names(mus) <- c(noscale_coefs, names(scale_mu))
+	} else {
+		sds <- scale_sd
+		mus <- scale_mu
+	}
+	sds <- sds[names(betahat)]
+	mus <- mus[names(betahat)]
+	if(grepl("(Intercept)", names(betahat))){
+		betahat[-1] <- betahat[-1]*sds[-1]
+		betahat[1] <- btemp[1] + sum(btemp[-1]*mus[-1])
+	} else {
+		sds <- sds[!names(sds) %in% "(Intercept)"]
+		betahat <- mus + betahat*sds
+	}
+	return(list(betahat))
+}
