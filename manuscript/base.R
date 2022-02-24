652a2ab2547f514495373e76454ed655652d693e
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-02-23 22:32:50 -0500

    research check

--- manuscript/base.R
+++ manuscript/base.R
@@ -2,13 +2,15 @@ library(shellpipes)
 library(dplyr)
 
 N <- 1e2
-rho = 0
 xbar = 2
 ybar = 0
+rho = 0.8
 
+## x, y and z
 b0 <- 5
 bx <- -0.4
 by <- 1.5
+bw <- 1
 
 seed <- 992
 
--- manuscript/sim.R
+++ manuscript/sim.R
@@ -6,19 +6,30 @@ loadEnvironments()
 set.seed(seed)
 
 ## Perfectly independent, normalized RVs (to perfectly match desired correlation, and probably partial correlations as well)
-r1 <- rnorm(N)
-r2 <- residuals(lm(rnorm(N)~r1))
-r3 <- rnorm(N)
-n1 <- (r1-mean(r1))/sd(r1)
-n2 <- (r2-mean(r2))/sd(r2)
+rx <- rnorm(N)
+ry <- rnorm(N)
+rw <- rnorm(N)
+rz <- rnorm(N)
+
+dy <- residuals(lm(ry~rx))
+dw <- residuals(lm(rw~rx))
+
+nx <- (rx-mean(rx))/sd(rx)
+ny <- (dy-mean(ry))/sd(dy)
+nw <- (dw-mean(rw))/sd(dw)
+nz <- (rz-mean(rz))/sd(rz)
 
 sim_df <- tibble(NULL
-	, x = n1+xbar
-	, y = rho*n1 + sqrt(1-rho^2)*n2+ybar
-	, lc = b0 + bx*x + by*y
-	, z = lc+r3
+	, x = nx+xbar
+	, y = rho*nx + sqrt(1-rho^2)*ny+ybar
+	, w = nw
+	, lc = b0 + bx*x + by*y + bw*w
+	, z = lc+nz
 )
 
 summary(sim_df)
+
+with(sim_df, cor.test(x, y))
+with(sim_df, cor.test(x, w))
 
 saveVars(sim_df)
