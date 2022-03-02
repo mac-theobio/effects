8bb9e4240c101d68d1d198617f2e43ff5f44a375
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-03-02 15:18:59 -0500

    fiddling

--- manuscript/anchorplot.R
+++ manuscript/anchorplot.R
@@ -1,5 +1,5 @@
 library(vareffects)
-library(magrittr)
+library(dplyr)
 library(ggplot2); theme_set(theme_bw())
 
 library(shellpipes)
@@ -10,6 +10,15 @@ objects()
 
 print(pipeStar())
 
+b <- bind_rows(NULL
+	, pred=as.data.frame(varpred(m, "x", isolate=FALSE))
+	, eff=as.data.frame(varpred(m, "x"))
+	, zero=as.data.frame(varpred(m, "x", isolate.value=0))
+	, .id="method"
+)
+
+summary(b)
+
 pred <- varpred(m, "x", isolate=FALSE, modelname="pred")
 eff <- varpred(m , "x", modelname="eff")
 zero <- varpred(m, "x", isolate.value=0, modelname="zero")
@@ -17,3 +26,4 @@ zero <- varpred(m, "x", isolate.value=0, modelname="zero")
 all <- list(pred, eff, zero) %>% combinevarpred
 
 plot(all)
+
