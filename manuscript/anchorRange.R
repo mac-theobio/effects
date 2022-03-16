aad22c9c9ad304e9a03d05764888db138f12c405
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-03-16 16:30:28 -0400

    fiddling hopelessly (with Bicko)

--- manuscript/anchorRange.R
+++ manuscript/anchorRange.R
@@ -5,21 +5,27 @@ library(dplyr)
 
 library(shellpipes)
 
+## Notes
+## Is it worth doing this?
+## If so, figure out how to make plots comparable, and put them side-by-side
+## Make w really orthogonal so that the fit is the same??
+
 set.seed(2113)
 N <- 25
 xbar <- 0
 ybar <- 0
-sig <- 10
-rhovals <- seq(0.1, 0.9, length.out=9)
+sig <- 1
+rhovals <- seq(0.1, 0.9, length.out=81)
 
 ## Perfectly independent, normalized RVs (to perfectly match desired correlation, and probably partial correlations as well)
 rx <- rnorm(N)
 ry <- rnorm(N)
 rw <- rnorm(N)
-rz <- rnorm(N)
 
 dy <- residuals(lm(ry~rx))
-dw <- rw
+dw <- residuals(lm(rw~rx))
+
+dy <- sig*dw+dy
 
 nx <- (rx-mean(rx))/sd(rx)
 ny <- (dy-mean(dy))/sd(dy)
@@ -28,7 +34,7 @@ nw <- (dw-mean(dw))/sd(dw)
 for (rho in rhovals){
 	sim_df <- tibble(NULL
 		, x = nx+xbar
-		, y = rho*nx + sqrt(1-rho^2)*ny+ sig*nw + ybar
+		, y = rho*nx + sqrt(1-rho^2)*ny + ybar
 		, w = nw
 	)
 	m <- lm(y ~ x, data=sim_df)
@@ -40,9 +46,9 @@ for (rho in rhovals){
 		+ scale_color_manual(
 			values = c("black", friendly_pal("contrast_three")[2:3])
 		)
+		+ labs(title="uni", subtitle=rho)
 	)
 }
-
 
 for (rho in rhovals){
 	sim_df <- tibble(NULL
@@ -59,6 +65,7 @@ for (rho in rhovals){
 		+ scale_color_manual(
 			values = c("black", friendly_pal("contrast_three")[2:3])
 		)
+		+ labs(title="multi", subtitle=rho)
 	)
 }
 
