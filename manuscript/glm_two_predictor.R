1f234f298a94d12c5b2cb7b14473276d37d3d4cd
cygubicko <cygu@aims.ac.za>
2022-06-16 20:50:54 -0400

    Autocommit

--- manuscript/glm_two_predictor.R
+++ manuscript/glm_two_predictor.R
@@ -8,7 +8,7 @@ set.seed(991)
 
 ############################################################################################
 # Global variables
-nHH_obs <- 1e4
+nHH_obs <- 1e3
 perHH <- 1
 
 ############################################################################################
@@ -18,17 +18,20 @@ perHH <- 1
 ### No interaction model
 
 #### Simulation
-glm_sim_df <- linearsim(nHH=nHH_obs
+glm_sim <- linearsim(nHH=nHH_obs
 	, perHH=perHH
 	, addnoiseGLM=FALSE
 	, noiseSD=1 # ignored if addnoiseGLM=FALSE
 	, form=~1+x1+x2
-	, betas = c(5, 0.5, 1.5)
+	, betas = c(2, 0.5, 1.5)
 	, pgausian=list(p=2,fun=rnorm, mean=0, sd=1)
 	, pcat=list(p=0)
 	, link_scale=FALSE
 	, vnames=c("age", "wealthindex", "status")
-)$data
+)
+glm_sim_betas <- glm_sim$betas
+print(glm_sim_betas)
+glm_sim_df <- glm_sim$data
 head(glm_sim_df)
 true_prop_df <- (glm_sim_df
 	%>% summarize_all(mean)
