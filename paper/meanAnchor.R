67abccec2e5079dccce3ee09d7539cba28205a91
Jonathan Dushoff <dushoff@mcmaster.ca>
2023-02-07 16:42:02 +0800

    do a vdtest

---
+++ paper/meanAnchor.R
@@ -0,0 +1,11 @@
+library(varpred)
+
+library(shellpipes)
+loadEnvironments()
+
+anchor <- mean(nitro)
+
+vpred <- as.data.frame(varpred(mod, "nitro", isolate=FALSE, steps=200, isolate.value=anchor))
+veff <- as.data.frame(varpred(mod, "nitro", steps=200, isolate.value=anchor))
+
+saveEnvironment()
