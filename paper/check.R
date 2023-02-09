1ea2a03eefbdf61f43f15c17142dd629ee113861
cygubicko <cygu@aims.ac.za>
2023-02-09 17:02:29 -0500

    Fixing focal interactions

--- paper/check.R
+++ paper/check.R
@@ -5,28 +5,40 @@ library(emmeans)
 library(shellpipes)
 loadEnvironments()
 
-## summary(mod)
-## summary(dat)
-
-mm <- colMeans(model.matrix(mod))
+mm <- model.matrix(mod)
+cm <- colMeans(mm)
 bb <- coefficients(mod)
 
-print(matrix(mm, nrow=1) %*% bb)
+print(matrix(cm, nrow=1) %*% bb)
 mean(dat$mass)
 
-veff <- varpred(mod, c("nitro")
-	, x.var="nitro"
-	, returnall=TRUE
-)
-mean(veff$pred$fit)
-plot(veff)
-veff <- varpred(mod, "nitro", bias.adjust="observed")
-mean(veff$pred$fit)
-quit()
+veff <- varpred(mod, "nitro", at=list(nitro=dat$nitro), returnall=TRUE)
 
-vpred <- as.data.frame(varpred(mod, "nitro", isolate=FALSE, steps=200))
-veff <- as.data.frame(varpred(mod, "nitro", steps=200))
-iveff <- as.data.frame(varpred(mod, "nitro", steps=200, input_vars=TRUE))
+# varpred
+## center
+vmm <- veff$raw$model.matrix
+head(vmm)
 
+cmm <- colMeans(vmm)
+
+print(matrix(cmm, nrow=1) %*% bb)
+
+## means
+colMeans(vmm)
+
+# data
+## means
+print(cm)
+
+l1 <- sweep(vmm, 2, cmm, FUN="/")
+l1 <- sweep(l1, 2, cm, FUN="*")
+print(l1)
+print(matrix(colMeans(l1), nrow=1) %*% bb)
+
+pred <- as.vector(l1 %*% bb)
+mean(pred)
+
+head(veff$m2)
+all.equal(veff$m2, vmm)
 saveEnvironment()
 
