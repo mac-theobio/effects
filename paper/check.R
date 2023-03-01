676e7eabd319a61522f88b858adcbd3fbb0d706e
cygubicko <cygu@aims.ac.za>
2023-03-01 03:36:11 -0500

    Meeting JD

--- paper/check.R
+++ paper/check.R
@@ -1,11 +1,22 @@
 library(varpred)
+library(ggplot2)
 library(dplyr)
 library(emmeans)
 
 library(shellpipes)
 loadEnvironments()
 
+summary(mod)
+
+mean_biomass <- mean(dat$mass)
+mean_nitro=mean(dat$nitro)
+
 vpred <- varpred(mod, "nitro")
+plot(vpred) + geom_point(aes(x=mean_nitro, y=mean_biomass, size=3))
+print(vpred)
+
+vpred <- varpred(mod, "nitro", isolate=FALSE)
+plot(vpred)
 print(vpred)
 
 names(mod)
